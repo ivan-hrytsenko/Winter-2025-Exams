33151f65f6f6ffcd86d2823b3386349e13ab79c7
ivan-hrytsenko <ivan.hrytsenko123@gmail.com>
2025-01-13 20:23:05 +0000

    change naming

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -3,15 +3,15 @@
 
 'use strict';
 
-const intersection = (object_1, object_2) => {
-  for (const attribute_name of Object.keys(object_1)) {
-    if (object_1[attribute_name] === object_2[attribute_name]) {
-      object_2[attribute_name] = object_1[attribute_name];
+const intersection = (object1, object2) => {
+  for (const key of Object.keys(object1)) {
+    if (object1[key] === object2[key]) {
+      object2[key] = object1[key];
     } else {
-      delete object_1[attribute_name];
+      delete object1[key];
     }
   }
-  return object_1;
+  return object1;
 };
 
 module.exports = intersection;
