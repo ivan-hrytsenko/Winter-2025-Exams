a7b9322f410a6b5670402db2d6aeb25c977b4924
ivan-hrytsenko <ivan.hrytsenko123@gmail.com>
2025-01-13 19:45:21 +0000

    simplify

--- Tasks/inc.js
+++ Tasks/inc.js
@@ -5,8 +5,8 @@
 
 const incNumbers = (object) => {
   for (const key of Object.keys(object)) {
-    if ((typeof object[key]).charAt(0).toUpperCase() === 'N') {
-      object[key] = object[key] + 1;
+    if (typeof object[key] === 'number') {
+      ++object[key];
     }
   }
   return object;
