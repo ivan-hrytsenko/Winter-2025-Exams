f0c97d3f60f7d10e4b06fbf07e27d644720c2a5b
ivan-hrytsenko <ivan.hrytsenko123@gmail.com>
2025-01-13 19:57:27 +0000

    add 'use strict'

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -1,6 +1,8 @@
 // Refactor following solution
 // Find an intersection of two dictionaries
 
+'use strict';
+
 intersection = function intersection(object_1, object_2) {
   first_keys = Object.keys(object_1);
   {
