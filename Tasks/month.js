ef4ffd27780eee27612440b87be218b0a076793b
ivan-hrytsenko <ivan.hrytsenko123@gmail.com>
2025-01-14 03:16:28 +0000

    fix declaration

--- Tasks/month.js
+++ Tasks/month.js
@@ -1,9 +1,9 @@
 // Refactor following solution
 // Get month number
 
-'use strict'
+'use strict';
 
-Months = [
+const Months = [
   'jan',
   'feb',
   'mar',
@@ -18,8 +18,8 @@ Months = [
   'dec',
 ];
 
-Month = (s) => {
-  l = Months.length;
+const Month = (s) => {
+  const l = Months.length;
   for (let i = 0; i < l; i++) {
     if (s.toLowerCase().startsWith(Months[i])) return i + 1;
   }
